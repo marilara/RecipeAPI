b8328e0c56d0a32314d500184e18f6c575ea1dcb
maricela <laram@mail.gvsu.edu>
2019-11-06 23:45:16 -0500

    first commit redo

---
+++ RecipeAPI/Models/Recipe.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeAPI.Models
+{
+    public class Recipe
+    {
+        public string Title { get; set; }
+        public string Href { get; set; }
+        public string Ingredients { get; set; }
+        public string Thumbnail { get; set; }
+
+        public Recipe()
+        {
+
+        }
+
+        public Recipe(JToken t)
+        {
+            this.Title = t["title"].ToString();
+            this.Href = t["href"].ToString();
+            this.Ingredients = t["ingredients"].ToString();
+            this.Thumbnail = t["thumbnail"].ToString();
+        }
+    }
+}
---
+++ RecipeAPI/Models/RecipeDAL.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace RecipeAPI.Models
+{
+    public class RecipeDAL
+    {
+        private static string baseUrl = "http://www.recipepuppy.com/api";
+
+        public static Recipe GetRecipe(string i="", string q="")
+        {
+            HttpWebRequest request = WebRequest.CreateHttp($"{baseUrl}/?i={i}&q={q}");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            // Convert response to a string
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            string APIText = rd.ReadToEnd();
+
+            JToken t = ParseJsonString(APIText);
+
+            Recipe r = new Recipe(t);
+
+            return r;
+        }
+
+        public static JToken ParseJsonString(string text)
+        {
+            JToken output = JToken.Parse(text);
+            return output;
+        }
+    }
+}
